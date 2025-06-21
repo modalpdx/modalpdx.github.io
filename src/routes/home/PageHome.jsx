a078f8d1f43f145894ac95a138f98fca0c6cc827
Erik Ratcliffe <canyonjam@gmail.com>
2025-06-20 20:26:28 -0700

    Color and content updates

--- src/routes/home/PageHome.jsx
+++ src/routes/home/PageHome.jsx
@@ -5,7 +5,21 @@ function PageHome() {
   return (
     <>
       <div>
-        <p className="sub-heading">When I grow up, I'll actually have content that doesn't suck. It may be a while. A long while. So, like, don't wait.</p>
+        <p className="sub-heading">When I grow up, I'll actually have content that doesn't suck. Until then, you get a list of current favorites:</p>
+
+        <ul>
+          <li><strong>Food:</strong> unsalted dry roasted peanuts (in the shell, natch)</li>
+          <li><strong>Music:</strong> the Mercy Street CD by A Reminiscent Drive</li>
+          <li><strong>Tech:</strong> Mac Mini M4, 16GB RAM, 256GB SSD, dock w/ extra HDMI and ports</li>
+          <li><strong>Cat:</strong> our boy cat, 10/10 points (our girl cat still has too much kitten in her, 6/10 points)</li>
+          <li><strong>Pen:</strong> tough one...gotta say my Pilot Decimo is leading the pack at the moment</li>
+          <li><strong>Ink:</strong> Pilot/Nakimi Black. Best workhorse ink evah. Fight me.</li>
+          <li><strong>Beer:</strong> Icicle Cheat Code from Leavenworth, WA which is not available anywhere near me</li>
+          <li><strong>NSAID:</strong> aspirin</li>
+          <li><strong>Martini:</strong> dirty</li>
+          <li><strong>Programming language:</strong> AWK (shut up)</li>
+          <li><strong>Ereader:</strong> hardware: Kindle, everything else: Kobo</li>
+        </ul>
       </div>
     </>
   )
